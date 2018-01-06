ba7d573500d96b243c9aa768cce2e0189aa40b87
Chris <chrisneilbrown@gmail.com>
2018-01-07 01:56:41 +1100

    animated player

--- Player.gd
+++ Player.gd
@@ -18,14 +18,10 @@ func _ready():
 func _physics_process(delta):
 	rotation_degrees = 0
 	if is_on_floor():
-		if $AnimatedSprite.animation != "running":
-			$AnimatedSprite.animation = "idle"
 		vel.y = 0
 	if is_on_ceiling():
-		$AnimatedSprite.animation = "fall"
 		vel.y = 0	
 	if Input.is_action_pressed("ui_up") && is_on_floor():
-		$AnimatedSprite.animation = "jump"
 		vel.y = -SPEED * delta
 		if vel.y < 425:
 			vel.y = -425
@@ -36,19 +32,23 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		facing = 1
 		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.animation = "running"
 		vel.x += SPEED * delta
 		if vel.x > 500:
 			vel.x = 500
 	if Input.is_action_pressed("ui_left"):
 		facing = -1
 		$AnimatedSprite.flip_h = true
-		$AnimatedSprite.animation = "running"
 		vel.x -= SPEED * delta
 		if vel.x < -500:
 			vel.x = -500
 	vel.x /= 1 + delta * 5
-	if $AnimatedSprite.animation != "idle" and vel.x < 20:
+	if vel.y < 0:
+		$AnimatedSprite.animation = "jump"
+	elif vel.y > 15:
+		$AnimatedSprite.animation = "fall"	
+	elif vel.x > 20 or vel.x < -20:
+		$AnimatedSprite.animation = "running"
+	else:
 		$AnimatedSprite.animation = "idle"
 	move_and_slide(vel,Vector2(0,-1))
 	var coli = get_slide_count()
