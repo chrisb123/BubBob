d9ef6aa9452250c073cc5b179114551dac6d2611
Chris <chrisneilbrown@gmail.com>
2018-01-07 10:51:42 +1100

    Revert "Revert "change pop events""
    This reverts commit b234159a658e2cbbfe431011033e859cdfc94e44, reversing
    changes made to af7c856a8a8a52ad59f772e1581a213089a0e7f3.

--- bubble1.gd
+++ bubble1.gd
@@ -9,6 +9,7 @@ var dying = false
 var player_pushing = false
 const LIFE_TIME = 10
 const pop_time = 0.75
+var emit_part = false
 
 func _ready():
 	$Life.wait_time = LIFE_TIME
@@ -32,18 +33,30 @@ func _on_Life_timeout():
 	$pop.interpolate_property($Sprite, 'scale', $Sprite.get_scale(), Vector2(1.5,1.5) , pop_time, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	$pop.start()
 	$pop/pop_time.start()
-	$Pop_Bubble.volume_db = -(randi()%20) - 10
 		
 	#queue_free()
 
 func _on_pop_tween_completed( object, key ):
+<<<<<<< HEAD
 	$Pop_Bubble.play()
+	if emit_part:
+		$Particles.emitting = true
+=======
+	$Pop_Timer.volume_db = -(randi()%20) - 5
+	$Pop_Timer.play()
+>>>>>>> b26e2d70a19a5b970fbc7ee631cf89280ab79e52
 	$pop/pop_time.stop()
 	$Sprite.hide()
 	$CollisionShape2D.disabled = true
 
+<<<<<<< HEAD
 
 func _on_Pop_Bubble_finished():
+	if emit_part:
+		yield(get_tree().create_timer(0.5),"timeout")
+=======
+func _on_Pop_Timer_finished():
+>>>>>>> b26e2d70a19a5b970fbc7ee631cf89280ab79e52
 	queue_free()
 
 func _on_pop_time_timeout():
@@ -54,7 +67,6 @@ func _on_pop_time_timeout():
 
 func _on_Float_timeout():
 	gravity_scale = -1
-
 
 func _on_AnimatedSprite_animation_finished():
 	get_colliding_bodies()
@@ -69,16 +81,28 @@ func killbub(pk):
 			if item.is_in_group("bubble") || item.is_in_group("enemy"):
 				item.killbub(pk)
 		$CollisionShape2D.disabled = true
-		$Sprite.hide()
+		#$Sprite.hide()
 		linear_damp = 10
 		#var anim = data.find_node("AnimatedSprite")AnimatedSprite
 		#Bubble expand when killed
+<<<<<<< HEAD
 		$pop.interpolate_property($Sprite, 'scale', $Sprite.get_scale(), Vector2(1.5,1.5) , pop_time, Tween.TRANS_QUAD, Tween.EASE_OUT)
 		$pop.start()
-		$pop.interpolate_property($Sprite, 'rotation_degrees', 0 , 360 , pop_time, Tween.TRANS_QUAD, Tween.EASE_OUT)	
+		#$pop.interpolate_property($Sprite, 'rotation_degrees', 0 , 360 , pop_time, Tween.TRANS_QUAD, Tween.EASE_OUT)	
+		#$Particles.emitting = true
+		emit_part = pk
+=======
+		#$pop.interpolate_property($Sprite, 'scale', $Sprite.get_scale(), Vector2(1.5,1.5) , pop_time, Tween.TRANS_QUAD, Tween.EASE_OUT)
+		#$pop.interpolate_property($Sprite, 'rotation_degrees', 0 , 360 , pop_time, Tween.TRANS_QUAD, Tween.EASE_OUT)	
+		#$pop.start()
 		$Particles.emitting = true
+		$Pop_Bubble.volume_db = -(randi()%20) - 5
+		$Pop_Bubble.play()
+>>>>>>> b26e2d70a19a5b970fbc7ee631cf89280ab79e52
 		#$AnimatedSprite.play()
-		
+
+func _on_Pop_Bubble_finished():
+	queue_free()
 #func popbub():
 
 #	if $pop/pop_time.is_stopped():
@@ -101,5 +125,8 @@ func _on_Bubble_body_entered( body ):
 func _on_Bubble_body_exited( body ):
 	if body.is_in_group("player"):
 		player_pushing = false
+
+
+
 
 
