dd515ecbb019c9c0e3c962c9d7a0d528d006725a
Chris <chrisneilbrown@gmail.com>
2018-01-18 09:42:56 +1100

    fixed fireball

--- Fireball.gd
+++ Fireball.gd
@@ -16,6 +16,12 @@ func _process():
 	var item2 = $Fireball.get_overlapping_areas()
 	print (item2.get_name())
 
+func _on_Area2D_body_entered( body ):
+	if body.is_in_group("player"):
+		print("player hit")
+	if body.is_in_group("tiles"):
+		print("tilemap hit")
+
 func _on_Area2D_area_entered( area ):
 	if area.is_in_group("bubble_area"):
 		area.get_parent()._on_Life_timeout()
