e26027d8cce63d210975b270352762e66b739cb0
Adrian <35069419+kaerin@users.noreply.github.com>
2018-03-09 10:37:21 +0100

    Game over calls _title() not _ready()

--- GameOver.gd
+++ GameOver.gd
@@ -21,7 +21,13 @@ func _ready():
 	remove_child(file_main)
 
 func _on_Start_pressed():
-	get_parent()._ready()
+	get_parent()._title()
+	Global_Vars.score = 0
+	Global_Vars.lives = Global_Vars.start_lives
+	Global_Vars.enemyn = 0
+	Global_Vars.waven = 0
+	Global_Vars.leveln = 0
+	Global_Vars.Osys = OS.get_name()
 	queue_free()
 	
 func _on_Quit_pressed():
